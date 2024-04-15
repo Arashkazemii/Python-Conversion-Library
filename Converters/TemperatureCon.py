0139991f3b83613675de56fd4b76529c587c8d49
ArashKazemi <kzmarsh19@gmail.com>
2024-04-15 12:45:05 +0330

    Temperature Converter Add, TemperatureCon.py

---
+++ Converters/TemperatureCon.py
@@ -0,0 +1,30 @@
+def tempCon(value, inScale, outScale) :
+    tempScaleDict = {'Celsius' : 'C', 'Fahrenheit' : 'F', 'Kelvin' : 'K'}
+    
+    if inScale == 'C' :
+        if outScale == 'F' :
+            return value * 9/5 + 32
+        elif outScale == 'K' :
+            return value + 273.15
+        else :
+            print("Undefined Scale")
+    
+    elif inScale == 'F' :
+        if outScale == 'C' :
+            return (value - 32) * 5/9
+        elif outScale == 'K' :
+            return (value - 32) * 5/9 + 273.15
+        else :
+            print("Undefined Scale")
+    
+    elif inScale == 'K' :
+        if outScale == 'C' :
+            return value - 273.15
+        elif outScale == 'F' :
+            return (value - 273.15) * 9/5 + 32
+        else :
+            print("Undefined Scale")
+    
+    else :
+        print("Undefined Scale")
+        
