db31eb5dc6187160ae0cd4424ef489b182109380
ArashKazemi <kzmarsh19@gmail.com>
2024-04-15 13:38:36 +0330

    Frequency Converter Add. FrequencyCon.py

---
+++ Converters/FrequencyCon.py
@@ -0,0 +1,43 @@
+def frequencyCon(value, inScale, outScale):
+    frequencyScaleDict = {'Hertz': 'Hz', 'Kilohertz': 'kHz', 'Megahertz': 'MHz', 'Gigahertz': 'GHz'}
+
+    if inScale == 'Hz':
+        if outScale == 'kHz':
+            return value * 0.001
+        elif outScale == 'MHz':
+            return value * 1e-6
+        elif outScale == 'GHz':
+            return value * 1e-9
+        else:
+            return value
+    elif inScale == 'kHz':
+        if outScale == 'Hz':
+            return value * 1000
+        elif outScale == 'MHz':
+            return value * 1e-3
+        elif outScale == 'GHz':
+            return value * 1e-6
+        else:
+            return value
+    elif inScale == 'MHz':
+        if outScale == 'Hz':
+            return value * 1e+6
+        elif outScale == 'kHz':
+            return value * 1000
+        elif outScale == 'GHz':
+            return value * 0.001
+        else:
+            return value
+    elif inScale == 'GHz':
+        if outScale == 'Hz':
+            return value * 1e+9
+        elif outScale == 'kHz':
+            return value * 1e+6
+        elif outScale == 'MHz':
+            return value * 1000
+        else:
+            return value
+    else:
+        return value
+    
+    
